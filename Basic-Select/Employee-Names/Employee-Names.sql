d2cc1b1c900687085f6b3b193f756a326ae55d4d
Leonardo <leosimoes.engcomp@gmail.com>
2023-04-04 13:20:40 -0300

    HackerRank - SQL - Basic Select - Employee Names

---
+++ Basic-Select/Employee-Names/Employee-Names.sql
@@ -0,0 +1,10 @@
+/**
+ * Author: Leonardo Simoes
+ * Date: 03/23/2023
+ * HackerRank - SQL - Basic Select - Employee Names
+ * Language: MySQL / MS SQL Server
+*/
+
+SELECT name
+FROM Employee
+ORDER BY 1;
